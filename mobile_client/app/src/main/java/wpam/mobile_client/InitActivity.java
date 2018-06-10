557bd87054ebe4fbc2068f36afc505d795c60166
HBuczynski <huber.buczynski94@gmail.com>
2018-06-10 11:56:23 +0200

    Added progress bar to the init activity.

--- mobile_client/app/src/main/java/wpam/mobile_client/InitActivity.java
+++ mobile_client/app/src/main/java/wpam/mobile_client/InitActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 
@@ -14,6 +15,7 @@ public class InitActivity extends AppCompatActivity {
     private TextView portNumber;
     private TextView warning;
     private Button connectButton;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +26,15 @@ public class InitActivity extends AppCompatActivity {
         portNumber = (TextView)findViewById(R.id.port_number);
         warning = (TextView)findViewById(R.id.warning_label);
         connectButton = (Button)findViewById(R.id.connect_button);
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.INVISIBLE);
 
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
             {
 
+                progressBar.setVisibility(View.VISIBLE);
                 String ip = ipAddress.getText().toString();
                 String port = portNumber.getText().toString();
 
@@ -38,6 +43,7 @@ public class InitActivity extends AppCompatActivity {
                 intent.putExtra("port", port);
 
                 startActivity(intent);
+                finish();
             }
         });
     }
