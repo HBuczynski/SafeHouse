7c59fcd3a86c5b0237cda666743fe0fb5a9fc250
HBuczynski <huber.buczynski94@gmail.com>
2018-06-11 19:10:28 +0200

    Updated blinds.

--- mobile_client/app/src/main/java/wpam/mobile_client/BlindActivity.java
+++ mobile_client/app/src/main/java/wpam/mobile_client/BlindActivity.java
@@ -1,6 +1,9 @@
 package wpam.mobile_client;
 
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.widget.ImageView;
 
@@ -10,17 +13,60 @@ public class BlindActivity extends PreferenceActivity {
 
     ClientThread client;
 
-    ImageView image;
+    CheckBoxPreference userOutHome;
+    CheckBoxPreference userInHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
 
-
         client = ClientThread.getInstance();
 
+        userOutHome = (CheckBoxPreference)findPreference("user_out_home");
+        userOutHome.setChecked(false);
+        userInHome = (CheckBoxPreference)findPreference("user_in_home");
+        userInHome.setChecked(false);
 
-
+        setListeners();
         }
+
+    private void setListeners()
+    {
+        userOutHome.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                CheckBoxPreference checkBoxPreference = (CheckBoxPreference)preference;
+                if(checkBoxPreference.isChecked())
+                {
+                    userInHome.setEnabled(true);
+                }
+                else
+                {
+                    userInHome.setEnabled(false);
+                }
+
+                return true;
+            }
+        });
+
+        userInHome.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+                CheckBoxPreference checkBoxPreference = (CheckBoxPreference)preference;
+                if(checkBoxPreference.isChecked())
+                {
+                    userOutHome.setEnabled(true);
+                }
+                else
+                {
+                    userOutHome.setEnabled(false);
+                }
+
+                return true;
+            }
+        });
+    }
 }
